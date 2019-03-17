74aa78a9865523e9966241a1fcd23ed508cffb8a
gregnau <gregnau@live.nl>
2019-03-17 18:16:11 +0100

    Different joystick mappings per channel

--- joystick.ino
+++ joystick.ino
@@ -14,19 +14,79 @@
     #define JOYSTICK_TYPE 0x04
   #endif
   
-  #define BUTTON_COUNT 2  // Indicates how many buttons will be available on the joystick. (0-32)
+  // Configure joystick features according to number of available channels
+  #if CHANNELS == 1
+    #define INCLUDE_X_AXIS true  // Indicates if the X Axis is available on the joystick
+    #define INCLUDE_Y_AXIS false  // Indicates if the Y Axis is available on the joystick
+    #define INCLUDE_Z_AXIS false  // Indicates if the Z Axis (in some situations this is the right X Axis) is available on the joystick
+    #define INCLUDE_RX_AXIS false  // Indicates if the X Axis Rotation (in some situations this is the right Y Axis) is available on the joystick
+    #define INCLUDE_RY_AXIS false  // Indicates if the Y Axis Rotation is available on the joystick
+    #define INCLUDE_RZ_AXIS false  // Indicates if the Z Axis Rotation is available on the joystick
+    #define BUTTON_COUNT 0  // Indicates how many buttons will be available on the joystick (0-32)
+  #elif CHANNELS == 2
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS false
+    #define INCLUDE_RX_AXIS false
+    #define INCLUDE_RY_AXIS false
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 0
+  #elif CHANNELS == 3
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS false
+    #define INCLUDE_RX_AXIS false
+    #define INCLUDE_RY_AXIS false
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 1
+  #elif CHANNELS == 4
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS true
+    #define INCLUDE_RX_AXIS true
+    #define INCLUDE_RY_AXIS false
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 0
+  #elif CHANNELS == 5
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS true
+    #define INCLUDE_RX_AXIS true
+    #define INCLUDE_RY_AXIS false
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 1
+  #elif CHANNELS == 6
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS true
+    #define INCLUDE_RX_AXIS true
+    #define INCLUDE_RY_AXIS true
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 1
+  #elif CHANNELS == 7
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS true
+    #define INCLUDE_RX_AXIS true
+    #define INCLUDE_RY_AXIS true
+    #define INCLUDE_RZ_AXIS false
+    #define BUTTON_COUNT 2
+  #elif CHANNELS == 8
+    #define INCLUDE_X_AXIS true
+    #define INCLUDE_Y_AXIS true
+    #define INCLUDE_Z_AXIS true
+    #define INCLUDE_RX_AXIS true
+    #define INCLUDE_RY_AXIS true
+    #define INCLUDE_RZ_AXIS true
+    #define BUTTON_COUNT 2
+  #endif
+
   #define HAT_SWITCH_COUNT 0  // Indicates how many hat switches will be available on the joystick. (0-2)
-  #define INCLUDE_X_AXIS true  // Indicates if the X Axis is available on the joystick.
-  #define INCLUDE_Y_AXIS true  // Indicates if the Y Axis is available on the joystick.
-  #define INCLUDE_Z_AXIS true  // Indicates if the Z Axis (in some situations this is the right X Axis) is available on the joystick.
-  #define INCLUDE_RX_AXIS true  // Indicates if the X Axis Rotation (in some situations this is the right Y Axis) is available on the joystick.
-  #define INCLUDE_RY_AXIS true  // Indicates if the Y Axis Rotation is available on the joystick.
-  #define INCLUDE_RZ_AXIS true  // Indicates if the Z Axis Rotation is available on the joystick.
-  #define INCLUDE_RUDDER true  // Indicates if the Rudder is available on the joystick.
-  #define INCLUDE_THROTTLE true  // Indicates if the Throttle is available on the joystick.
-  #define INCLUDE_ACCELERATOR true  // Indicates if the Accelerator is available on the joystick.
-  #define INCLUDE_BRAKE true  // Indicates if the Brake is available on the joystick.
-  #define INCLUDE_STEERING true  // Indicates if the Steering is available on the joystick.
+  #define INCLUDE_RUDDER false  // Indicates if the Rudder is available on the joystick.
+  #define INCLUDE_THROTTLE false  // Indicates if the Throttle is available on the joystick.
+  #define INCLUDE_ACCELERATOR false  // Indicates if the Accelerator is available on the joystick.
+  #define INCLUDE_BRAKE false  // Indicates if the Brake is available on the joystick.
+  #define INCLUDE_STEERING false  // Indicates if the Steering is available on the joystick.
 
   // Joystick configuration (manual in src/ArduinoJoystickLibrary/README.md)
   Joystick_ Joystick(HID_REPORT_ID, JOYSTICK_TYPE, BUTTON_COUNT, HAT_SWITCH_COUNT,
@@ -58,35 +118,138 @@
       // Process every channel with a flag
       for (uint8_t i=0;i<CHANNELS;i++) {
         if (localflags & FLAGS[i]) {
-          switch (i) {
-            case 0:  // CH 1
-              Joystick.setXAxis(rc_values[i]);
-              break;
-            case 1:  // CH 2
-              Joystick.setYAxis(rc_values[i]);
-              break;
-            case 2:  // CH 3
-              Joystick.setZAxis(rc_values[i]);
-              break;
-            case 3:  // CH 4
-              Joystick.setRzAxis(rc_values[i]);
-              break;
-            case 4:  // CH 5
-              Joystick.setRyAxis(rc_values[i]);
-              break;
-            case 5:  // CH 6
-              Joystick.setRxAxis(rc_values[i]);
-              break;
-
-            #if CHANNELS > 6
+          #if CHANNELS == 1
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+            }
+          #elif CHANNELS == 2
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+            }
+          #elif CHANNELS == 3
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setButton(0, rc_values[i] < STICK_CENTER ? 0 : 1);
+                break;
+            }
+          #elif CHANNELS == 4
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setZAxis(rc_values[i]);
+                break;
+              case 3:  // CH 4
+                Joystick.setRxAxis(rc_values[i]);
+                break;
+            }
+          #elif CHANNELS == 5
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setZAxis(rc_values[i]);
+                break;
+              case 3:  // CH 4
+                Joystick.setRxAxis(rc_values[i]);
+                break;
+              case 4:  // CH 5
+                Joystick.setButton(0, rc_values[i] < STICK_CENTER ? 0 : 1);
+                break;
+            }
+          #elif CHANNELS == 6
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setZAxis(rc_values[i]);
+                break;
+              case 3:  // CH 4
+                Joystick.setRxAxis(rc_values[i]);
+                break;
+              case 4:  // CH 5
+                Joystick.setRyAxis(rc_values[i]);
+                break;
+              case 5:  // CH 6
+                Joystick.setButton(0, rc_values[i] < STICK_CENTER ? 0 : 1);
+            }
+          #elif CHANNELS == 7
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setZAxis(rc_values[i]);
+                break;
+              case 3:  // CH 4
+                Joystick.setRxAxis(rc_values[i]);
+                break;
+              case 4:  // CH 5
+                Joystick.setRyAxis(rc_values[i]);
+                break;
+              case 5:  // CH 6
+                Joystick.setButton(0, rc_values[i] < STICK_CENTER ? 0 : 1);
+                break;
+              case 6:  // CH 7
+                Joystick.setButton(1, rc_values[i] < STICK_CENTER ? 0 : 1);
+                break;
+            }
+          #elif CHANNELS == 8
+            switch (i) {
+              case 0:  // CH 1
+                Joystick.setXAxis(rc_values[i]);
+                break;
+              case 1:  // CH 2
+                Joystick.setYAxis(rc_values[i]);
+                break;
+              case 2:  // CH 3
+                Joystick.setZAxis(rc_values[i]);
+                break;
+              case 3:  // CH 4
+                Joystick.setRxAxis(rc_values[i]);
+                break;
+              case 4:  // CH 5
+                Joystick.setRyAxis(rc_values[i]);
+                break;
+              case 5;  // CH 6
+                Joystick.setRzAxis(rc_values[i]);
+                break;
               case 6:  // CH 7
                 Joystick.setButton(0, rc_values[i] < STICK_CENTER ? 0 : 1);
                 break;
               case 7:  // CH 8
                 Joystick.setButton(1, rc_values[i] < STICK_CENTER ? 0 : 1);
                 break;
-            #endif
-          }
+            }
+          #endif
         }
       }
   
